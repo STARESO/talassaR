2bd8cac17703cd6d7c58f5d46ff21200caea3776
aubin_woehrel <73119009+awoehrel@users.noreply.github.com>
2026-02-12 16:50:38 +0100

    improvement talassa formatage

--- main/formatage_talassa.R
+++ main/formatage_talassa.R
@@ -42,6 +42,7 @@ library("rlang")
 
 ## Ressources locales ----
 source("r/paths.R")
+source("r/fct_category_map.R")
 
 ## Import des données ----
 
@@ -53,9 +54,16 @@ codes_talassa <- read.xlsx(
   startRow = 2
 )
 
-# Activités
+# Activités format observatoire corrigé
+survolusage_obs <- st_read(paths$processed_obs_survolusage)
+peche_obs <- st_read(paths$processed_obs_peche)
+donia_obs <- st_read(paths$processed_obs_donia)
 plongee_obs <- st_read(paths$processed_obs_plongee)
 
+# Délimitation PNMCCA
+pnm_borders <- sf::st_read(paths$raw_pnmcca_borders) %>%
+  sf::st_transform(crs = 4326) %>%
+  dplyr::filter(NOM_SITE == "cap Corse et Agriate")
 
 # Transformations initiales ----
 codes_talassa <- codes_talassa %>%
@@ -65,12 +73,21 @@ codes_talassa <- codes_talassa %>%
 
 View(codes_talassa)
 
+
 # Modification activites ----
 
 ## Modification survols usages ----
+
+
 ## Modification survols plaba ----
+
+
 ## Modification plongee ----
-unique(plongee_obs$resoblo_code_n1) %in% codes_talassa$code_resoblo_plus_proche
+plongee_bool <- unique(plongee_obs$resoblo_code_n1) %in% codes_talassa$code_resoblo_plus_proche
+
+if (sum(!plongee_bool) != 0) {
+  simpleWarning("Codes Talassa plongée non valides, données non enregistrées.")
+}
 
 plongee_talassa <- codes_talassa %>%
   select(code_resoblo_plus_proche, talassa_code, talassa_intitule) %>%
@@ -79,9 +96,42 @@ plongee_talassa <- codes_talassa %>%
 plongee_talassa <- plongee_talassa %>%
   select(-c(resoblo_intitule_n1, resoblo_code_n1))
 
+
 ## Modification peche ----
+
+
 ## Modification donia ----
 
+# map_taille <- category_map(donia_obs, "taille")
+# map_taille
+
+# map_region <- category_map(donia_obs, "region")
+# map_region
+
+# Check bateaux sans taille
+donia_obs %>%
+  filter(is.na(taille)) %>%
+  dim(.) # 195 entités sans taille
+
+# Elimination des bateaux sans taille
+donia_obs <- donia_obs %>%
+  filter(!is.na(taille))
+
+# Check presence
+donia_bool <- unique(donia_obs$resoblo_code) %in% codes_talassa$code_resoblo_plus_proche
+
+
+if (sum(!donia_bool) != 0) {
+  simpleWarning("Codes Talassa Donia non valides, données non enregistrées.")
+
+  check_donia <- donia_obs %>%
+    st_drop_geometry() %>%
+    filter(resoblo_code %in% unique(donia_obs$resoblo_code)[!donia_bool]) %>%
+    select(resoblo_intitule, resoblo_code, resoblo_niveau) %>%
+    distinct()
+
+  View(check_donia)
+}
 
 # Modification habitats ----
 
@@ -95,3 +145,5 @@ st_write(
   driver = "GPKG",
   append = FALSE
 )
+
+st_write()

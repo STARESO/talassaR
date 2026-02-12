2a2908a0b42a16107379e82f99bb65f9399df4d0
aubin_woehrel <73119009+awoehrel@users.noreply.github.com>
2026-02-12 16:51:36 +0100

    simplifying to new architecture format : outputs resoblo cleaned format

--- main/correction_donia.R
+++ main/correction_donia.R
@@ -24,13 +24,13 @@ rm(list = ls())
 
 ## Import des librairies ----
 
-# Data import and tidying
+# Lecture et manipulations de données
 library("readr")
 library("dplyr")
 library("tidyr")
 library("stringr")
 
-# Data representations
+# Représentations
 library("ggplot2")
 library("ggbeeswarm")
 library("ggExtra")
@@ -41,12 +41,14 @@ library("sf")
 library("leaflet")
 library("rlang")
 
-## Sourcing local resources ----
+## Ressources locales ----
 source("r/paths.R")
-source("r/fct_category_map.R") # Custom data spatial map function
+source("r/fct_category_map.R") # Fonction personnalisée carto R
 
-## Importing data ----
 
+## Import des données ----
+
+# Donia
 donia <- read_delim(
   paths$raw_donia,
   delim = ";",
@@ -54,18 +56,20 @@ donia <- read_delim(
   trim_ws = TRUE
 )
 
+# Délimitation PNMCCA
 pnm_borders <- sf::st_read(paths$raw_pnmcca_borders) %>%
   sf::st_transform(crs = 4326) %>%
   dplyr::filter(NOM_SITE == "cap Corse et Agriate")
 
-# Preprocess ----
 
-## First investigations ----
+# Investigation et modifs initiales ----
+
+## Check structure ----
 spec(donia)
 skimr::skim(donia)
 names(donia)
 
-## Small transformations ----
+## Modifs initiales structure ----
 donia <- donia %>%
   separate_wider_delim(
     col = date_mouillage_central_european_time,
@@ -86,12 +90,15 @@ donia <- donia %>%
     lat_y = latitude_position_degres_decimaux
   )
 
-## Structure verifications ----
+## Check structure 2 ----
+
+# Récurrence observations noms de bateaux
 t1 <- donia %>%
   group_by(nom) %>%
   summarize(n = n()) %>%
   arrange(nom, desc(n))
 
+# Nombre bateaux nommées vs bateaux sans nom
 t1sum <- t1 %>%
   mutate(type = case_when(
     is.na(nom) ~ "unnamed",
@@ -100,123 +107,118 @@ t1sum <- t1 %>%
   group_by(type) %>%
   summarize(n = sum(n, na.rm = FALSE))
 
-t1sum
+t1sum # 1628 bateaux sans noms
+t1sum$n[2] * 100 / sum(t1sum$n) # Proportion 16.5% de bateaux sans noms
 
+# Récurrence bateaux uniques par jour ?
 t2 <- donia %>%
   group_by(nom, date) %>%
   summarize(n = n()) %>%
   arrange(nom, desc(n), date)
 
+# Récupération des types uniques de navires pour lien RESOBLO
 type_navire <- donia %>%
   select(type_navire, type_navire_brut) %>%
   group_by_all() %>%
   summarize(n = n()) %>%
   arrange(desc(type_navire), desc(n))
 
-write.csv2(type_navire, paths$output_donia_type_navire)
+# Sauvegarde csv types navires -> pour création reférence resoblo-donia
+write.csv2(type_navire, paths$processed_donia_type_navire)
 
-type_na_vs_value <- type_navire %>%
-  mutate(type_navire_brut = case_when(
-    is.na(type_navire_brut) ~ "NA",
+# Nombre de navires avec vs sans type défini
+type_unknown <- type_navire %>%
+  mutate(type_navire = case_when(
+    type_navire == "Unknown" ~ "type_absent",
+    is.na(type_navire) ~ "type_absent",
     TRUE ~ "type_existe"
   )) %>%
-  group_by(type_navire_brut) %>%
+  group_by(type_navire) %>%
   summarize(n = sum(n, na.rm = FALSE))
 
+type_unknown # 6142 bateaux sans type !
+type_unknown$n[1] * 100 / sum(type_unknown$n) # 62% des données inutilisables
+type_unknown$n[2] * 100 / sum(type_unknown$n) # Donc seulement 38% utilisables...
+
+# Infos masses d'eau
 unique(donia$nom_masse_eau)
 
-## Modifications post-investigation
-
+# Contenu colonnes texte vers minuscules
 donia <- donia %>%
   mutate(across(
     c(type_navire_brut, type_navire, nom_masse_eau, region),
     str_to_lower
   ))
 
-type_navire <- donia %>%
-  select(type_navire, type_navire_brut) %>%
-  group_by_all() %>%
-  summarize(n = n())
 
+# Investigation visuelle cartographique ----
 
-# Spatialisation ----
+# Transfo vers format spatial
 donia_spatial <- st_as_sf(donia, coords = c("lon_x", "lat_y"), crs = 4326)
 
-## Variable distinction map ----
-# Variable per variable
-map_region <- category_map(donia_spatial, "region")
-map_region
+## Visualisation par variables ----
 
-map_type <- category_map(donia_spatial, "type_navire")
-map_type
+carto <- FALSE
 
-map_sous_type <- category_map(donia_spatial, "type_navire_brut")
-map_sous_type
+if (carto) {
+  # Création des cartes via fonction personnalisée
+  map_region <- category_map(donia_spatial, "region")
+  map_type <- category_map(donia_spatial, "type_navire")
+  map_sous_type <- category_map(donia_spatial, "type_navire_brut")
+  map_annee <- category_map(donia_spatial, "annee_mouillage")
+  map_masse_eau <- category_map(donia_spatial, "code_masse_eau")
+  map_proba_impact <- category_map(donia_spatial, "probabilite_impact", compressor = 10)
+  map_duree_mouillage <- category_map(donia_spatial, "duree_mouillage", compressor = 100)
+  map_taille <- category_map(donia_spatial, "taille", compressor = 1)
 
-map_annee <- category_map(donia_spatial, "annee_mouillage")
-map_annee
+  # Visualisation des cartes interactives (choix lancement ligne pour visualisation)
+  map_region
+  map_type
+  map_sous_type
+  map_annee
+  map_masse_eau
+  map_proba_impact
+  map_duree_mouillage
+  map_taille
 
-map_masse_eau <- category_map(donia_spatial, "code_masse_eau")
-map_masse_eau
+  ## Visualisations des types de navires ----
 
-map_proba_impact <- category_map(donia_spatial, "probabilite_impact", compressor = 10)
-map_proba_impact
+  # Cargos
+  donia_spatial %>%
+    filter(type_navire_brut %in% c("cargo ship", "general cargo ship")) %>%
+    category_map(., "type_navire_brut")
 
-map_duree_mouillage <- category_map(donia_spatial, "duree_mouillage", compressor = 100)
-map_duree_mouillage
+  # Transport de passagers
+  donia_spatial %>%
+    filter(type_navire_brut %in% c("passenger ship", "passenger (cruise) ship", "passenger/ro-ro cargo ship")) %>%
+    category_map(., "type_navire_brut")
 
-map_taille <- category_map(donia_spatial, "taille", compressor = 1)
-map_taille
+  # Plongée
+  donia_spatial %>%
+    filter(type_navire_brut %in% c("diving ops")) %>%
+    category_map(., "type_navire_brut")
+
+  # Entrainement
+  donia_spatial %>%
+    filter(type_navire_brut %in% c("training ship")) %>%
+    category_map(., "type_navire_brut")
+
+  # Yacht
+  donia_spatial %>%
+    filter(type_navire_brut %in% c("yacht")) %>%
+    category_map(., "taille")
+}
 
 
-# Checking per type :
+# Ajout infos RESOBLO ----
 
-# Cargo ships
-donia_spatial %>%
-  filter(type_navire_brut %in% c(
-    "cargo ship", "general cargo ship"
-  )) %>%
-  category_map(., "type_navire_brut")
+## Jointure codes ----
 
-# Passenger ships
-donia_spatial %>%
-  filter(type_navire_brut %in% c(
-    "passenger ship",
-    "passenger (cruise) ship",
-    "passenger/ro-ro cargo ship"
-  )) %>%
-  category_map(., "type_navire_brut")
-
-# Diving ops
-donia_spatial %>%
-  filter(type_navire_brut %in% c(
-    "diving ops"
-  )) %>%
-  category_map(., "type_navire_brut")
-
-# Training ship
-donia_spatial %>%
-  filter(type_navire_brut %in% c(
-    "training ship"
-  )) %>%
-  category_map(., "type_navire_brut")
-
-# Yacht
-donia_spatial %>%
-  filter(type_navire_brut %in% c(
-    "yacht"
-  )) %>%
-  category_map(., "taille")
-
-# Resoblo format ----
-
-## Joining datasets ----
-
-# Import of donia-resoblo connections reference manually completed
+# Import référence liens codes Resoblo - Donia
 donia_resoblo <- read.csv2(paths$processed_donia_resoblo) %>%
   select(-type_navire)
 
-# Adding generalist columns of code and intitule depending on level
+# Ajout colonne code Resoblo niveau le plus précis
 donia_resoblo <- donia_resoblo %>%
   mutate(
     resoblo_intitule = case_when(
@@ -233,18 +235,18 @@ donia_resoblo <- donia_resoblo %>%
     )
   )
 
-# New version of donia with resoblo info by left join
+# Jointure codes Resoblo à Donia
 donia <- donia %>%
-  filter(!is.na(type_navire_brut)) %>% # All non specified type_navire_brut are removed
+  filter(!is.na(type_navire_brut)) %>% # Navires sans type brut enlevés
   left_join(., donia_resoblo, by = join_by("type_navire_brut"))
 
-# Checking amount of observations per resoblo level 2
+# Check nombre observations niveau 2
 t1 <- donia %>%
   select(resoblo_code_n2, resoblo_intitule_n2) %>%
   group_by_all() %>%
   summarize(n = n())
 
-# All NA for resoblo at level 2
+# Catégories RESOBLO avec NA au niveau 2
 t2 <- donia %>%
   filter(is.na(resoblo_code_n2)) %>%
   select(
@@ -256,11 +258,11 @@ t2 <- donia %>%
   summarize(n = n()) %>%
   arrange(desc(n))
 
-# Filter out all lines without resoblo correspondance from dataset
+# Elimination des entités sans code RESOBLO existant
 donia <- donia %>%
   filter(!is.na(resoblo_intitule_n2))
 
-# Checking left over
+# Check type entités restantes
 t3 <- donia %>%
   select(
     type_navire,
@@ -276,9 +278,12 @@ t3 <- donia %>%
   arrange(desc(n)) %>%
   ungroup()
 
-skimr::skim(donia) # A few boats without any size
+View(t3)
 
-# Therefore, trying to see which categories do not have size :
+# Recheck complétion Donia
+skimr::skim(donia) # Quelques bateaux sans taille !
+
+# Check catégories sans tailles
 t4 <- donia %>%
   filter(is.na(taille)) %>%
   select(
@@ -291,7 +296,7 @@ t4 <- donia %>%
   arrange(desc(n_na)) %>%
   ungroup()
 
-# And then computing completion rate :
+# Taux de complétion taille par catégorie
 t4 <- t3 %>%
   filter(type_navire_brut %in% t4$type_navire_brut) %>%
   select(type_navire_brut, n) %>%
@@ -306,59 +311,28 @@ t4 <- t3 %>%
   select(type_navire_brut, resoblo_intitule, n, n_na, taux_completion_taille) %>%
   rename(na_taille = n_na)
 
+View(t4)
+sum(t4$na_taille) # 195 bateaux sans taille
 
-# Graphical representations of size of boats and time anchored for transfo thinking process
-ggplot(donia, aes(x = taille, y = "")) +
-  geom_beeswarm(method = "center") +
-  theme_pubr() +
-  labs(y = "")
-
-ggplot(donia, aes(x = duree_mouillage, y = "")) +
-  geom_beeswarm(method = "center") +
-  theme_classic() +
-  labs(y = "")
-
-g1 <- ggplot(donia, aes(x = taille, y = duree_mouillage)) +
-  geom_point(size = 1) +
-  theme_pubr() +
-  scale_y_reverse()
-ggMarginal(g1, type = "histogram", size = 3)
-
-g2 <- ggplot(donia, aes(x = taille, y = duree_mouillage)) +
-  geom_point(size = 1) +
-  theme_pubr() +
-  scale_y_continuous(transform = scales::compose_trans("log10", "reverse"))
-ggMarginal(g2, type = "histogram", size = 3)
-
-g3 <- ggplot(donia, aes(x = taille, y = duree_mouillage)) +
-  geom_point(size = 1) +
-  theme_pubr() +
-  scale_x_continuous(transform = "log10") +
-  scale_y_continuous(transform = scales::compose_trans("log10", "reverse"))
-ggMarginal(g3, type = "histogram", size = 3)
-
-g4 <- ggplot(donia, aes(x = taille, y = duree_mouillage)) +
-  geom_point(size = 1) +
-  theme_pubr() +
-  scale_x_continuous(transform = "log10", limits = c(10, 200)) +
-  scale_y_continuous(transform = scales::compose_trans("log10", "reverse"))
-ggMarginal(g4, type = "histogram", size = 3)
+View(donia)
+skimr::skim(donia)
 
 
-# Spatialisation post-fusion check ----
+# Dernières modifications
+donia <- donia %>%
+  rename(nom_bateau = nom) %>%
+  relocate(c(resoblo_intitule, resoblo_code), .after = nom_bateau)
 
+
+# Spatialisation ----
+
+# Spatialisation WSG84
 donia_spatial <- st_as_sf(donia, coords = c("lon_x", "lat_y"), crs = 4326)
 
-# Exporting donia corrected data as gpkg format
+# Export format gpkg
 st_write(
-  donia_spatial,
-  paste0(paths$processed_obs_donia),
+  obj = donia_spatial,
+  dsn = paste0(paths$processed_obs_donia),
   driver = "GPKG",
   append = FALSE
 )
-
-map_taille <- category_map(donia_spatial, "taille", compressor = 1)
-map_taille
-
-map_region <- category_map(donia_spatial, "region")
-map_region

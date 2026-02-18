5db072ca6be46bfdfdbca23cc10ef0da5d1c85c8
aubin_woehrel <73119009+awoehrel@users.noreply.github.com>
2026-02-18 14:21:16 +0100

    refactoring

--- main/correction_survols_check2.R
+++ main/correction_survols_check2.R
@@ -1,68 +1,69 @@
 #' ---
-#' title : "talassaR - survols_gpkg_check"
+#' title : "talassaR - correction survols check2"
 #' author : Aubin Woehrel
 #' creation date : 2025-11-18
 #' ---
 #'
 #' =============================================================================
 #'
-#' talassaR :
-#' Survols gpkg check
+#' talassaR : Correction survols check n2
 #'
 #' Description :
-#' Small script to check a second time the survols usages data after manual
-#' correction in QGIS.
+#' Script permettant de faire la seconde étape de check des données de survol
+#' des usages (plan d'eau) du PNMCCA.
 #'
 #' =============================================================================
 
-# Initialization ----
+# Initialisation ----
 
-## Clean up and working directory ----
+## Nettoyage ----
 rm(list = ls())
 
-## Library imports ----
+## Import des librairies ----
 
-# Data import and tidying
+# Data: Import et manipulations
 library("readr")
 library("dplyr")
 library("tidyr")
 library("stringr")
 library("purrr")
 
-# Data exploration
-library("skimr")
-
-# Data representations
+# Data: représentations
 library("ggplot2")
 
-# Spatial
+# Data: spatial
 library("sf")
 
-## Sourcing local resources ----
+## Ressources locales ----
 source("r/paths.R")
 
-## Importing data ----
+## Import des données ----
 
+# liste des fichiers spatiaux corrigés
 file_names <- list.files(paths$processed_survols_corrected, full.names = TRUE)
 
-survols_all <- file_names %>%
+# compilation des fichiers spatiaux corrigés
+survols_resoblo <- file_names %>%
   map(st_read, quiet = TRUE) %>%
   bind_rows()
 
-# General check ----
-skim(survols_all)
 
-resoblo_verif <- survols_all %>%
+# Check général  ----
+
+# Structure
+skimr::skim(survols_resoblo)
+
+# Verification des codes et intitulés resoblo
+verif_codes_resoblo <- survols_resoblo %>%
   st_drop_geometry() %>%
   select(resoblo_code, resoblo_intitule) %>%
   group_by(resoblo_code, resoblo_intitule) %>%
   summarise(n = n()) %>%
   ungroup()
 
-
-## Saving mistakes files by identifying bad correspondence of names and codes
-# If mistakes, give line number of the object to check in source data
-# with line numbers corresponding to resoblo_verif dataset
+# Check des erreurs en identifiant les mauvaises correspondances de noms et codes.
+# En cas d'erreurs, check du numéro de ligne avec erreur à vérifier dans le fichier source
+# et numéro de ligne correspond à celui dans verif_codes_resoblo
 
 slice_flag <- FALSE
 
@@ -70,25 +71,37 @@ if (slice_flag) {
   to_slice <- c(1, 3, 4, 6, 12, 14, 16, 20, 31, 39)
   to_slice <- c(13)
 
-  resoblo_to_correct <- resoblo_verif %>%
+  resoblo_to_correct <- verif_codes_resoblo %>%
     slice(to_slice) %>%
     mutate(key_resoblo = str_c(resoblo_code, resoblo_intitule, sep = "_"))
 
-  to_correct <- survols_all %>%
+  to_correct <- survols_resoblo %>%
     st_drop_geometry() %>%
     mutate(key_resoblo = stringr::str_c(resoblo_code, resoblo_intitule, sep = "_")) %>%
     right_join(., resoblo_to_correct) %>%
     select(id_acti, date, resoblo_code, resoblo_intitule, key_resoblo)
 }
 
-sf::st_crs(survols_all)
-# Exporting total spatial dataset ----
+# Check du crs
+sf::st_crs(survols_resoblo) # 4326
 
+# Changement position colonnes
+survols_resoblo <- survols_resoblo %>%
+  relocate(resoblo_intitule, resoblo_code, .after = id_acti)
+
+# Suppression colonnes inutiles
+survols_resoblo <- survols_resoblo %>%
+  select(-c(
+    nom_acti,
+    act,
+    categorie_usage,
+    cod_act
+  ))
+
+# Exporting final format spatial resoblo ----
 st_write(
-  obj = survols_all,
-  dsn = paste0(
-    paths$processed_survols_corrected,
-    "us_med_pnmcca_observatoire_survols_usages_ofb_pt_4326.gpkg"
-  ),
-  delete_dsn = TRUE
+  obj = survols_resoblo,
+  dsn = paths$processed_obs_survolusage,
+  driver = "GPKG",
+  append = FALSE
 )

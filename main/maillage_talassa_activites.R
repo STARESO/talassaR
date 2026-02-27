48cd01fc768523f34a9cb0c48ce3d7c43f3330d3
aubin_woehrel <73119009+awoehrel@users.noreply.github.com>
2026-02-27 16:57:02 +0100

    start of maillage processed implementation

--- main/maillage_talassa_activites.R
+++ main/maillage_talassa_activites.R
@@ -14,6 +14,127 @@
 #'
 #' =============================================================================
 
+# Initialisation ----
+
+## Nettoyage ----
+rm(list = ls())
+
+## Import des librairies ----
+
+# Manipulations de données
+library("dplyr")
+library("tidyr")
+library("stringr")
+library("purrr")
+library("openxlsx")
+
+# Spatial
+library("sf")
+
+## Ressources locales ----
+source("r/paths.R")
+source("r/fct_jointure_id.R")
+
+
+## Import des données -----
+
+# Activités
+survolus_talassa <- st_read(paths$processed_tal_survolusage)
+peche_talassa <- st_read(paths$processed_tal_peche)
+donia_talassa <- st_read(paths$processed_tal_donia)
+plongee_talassa <- st_read(paths$processed_tal_plongee)
+
+# Carroyages
+carroyage_hex <- st_read(paths$raw_carroyage_final) %>%
+  st_transform(., crs = 4326)
+
+# Precheck ----
+names(survolus_talassa)
+names(peche_talassa)
+names(donia_talassa)
+names(plongee_talassa)
+
+
+# Liste des jeux de données et leurs noms
+data_list <- list(
+  list(df = survolus_talassa, source = "survol_usage"),
+  list(df = peche_talassa, source = "peche"),
+  list(df = donia_talassa, source = "donia"),
+  list(df = plongee_talassa, source = "plongee")
+)
+
+# Fonction pour obtenir les uniques des variables par jeux de données
+process_df <- function(df, source) {
+  vars <- df %>%
+    st_drop_geometry() %>%
+    select(-contains("talassa")) %>%
+    names() %>%
+    str_flatten(collapse = ", ")
+
+  df %>%
+    st_drop_geometry() %>%
+    count(talassa_code, talassa_intitule) %>%
+    mutate(
+      source = source,
+      variables = vars
+    )
+}
+
+# Application de la fonction pour comparer les codes et variables à disposition
+# dans l'intégralité des jeux de données à disposition
+all_codes <- data_list %>%
+  map(~ process_df(.x$df, .x$source)) %>%
+  list_rbind() %>%
+  arrange(talassa_code)
+
+
+# Export des combinaisons de codes et jeux de données en format xlsx.
+all_codes %>%
+  select(-n) %>%
+  mutate(
+    formule = "inserer formule",
+    ic = "inserer interfalle de confiance"
+  ) %>%
+  relocate(variables, .after = last_col()) %>%
+  write.xlsx(
+    x = .,
+    file = paths$raw_devcarroyage_activites
+  )
+
+# Permet de compléter manuellement l'excel avec les formules de calcul
+# d'agrégation et les intervalles de confiance pour la suite des étapes.
+# Reprendre la suite après complétion de la référence puis renomer selon
+# le chemin paths$raw_refcarroyage_activites
+paths$raw_refcarroyage_activites
+
+
+# Jointure ID maillage ----
+
+resultats_carroyage <- map(
+  .x = list(survolus_talassa, peche_talassa, donia_talassa, plongee_talassa),
+  .f = ~ jointure_id(carroyage = carroyage_hex, data = .x, id_name = "id_hex")
+)
+
+survolus_carroyage <- resultats_carroyage[[1]]
+peche_carroyage <- resultats_carroyage[[2]]
+donia_carroyage <- resultats_carroyage[[3]]
+plongee_carroyage <- resultats_carroyage[[4]]
+
+
+dim(survolus_talassa)
+dim(survolus_carroyage)
+
+dim(peche_talassa)
+dim(peche_carroyage)
+
+dim(donia_talassa)
+dim(donia_carroyage)
+
+dim(plongee_talassa)
+dim(plongee_carroyage)
+
+
+
 
 ## Réflexion transfos donia ----
 ggplot(donia_obs, aes(x = taille, y = "")) +
@@ -51,9 +172,6 @@ g4 <- ggplot(donia_obs, aes(x = taille, y = duree_mouillage)) +
   scale_x_continuous(transform = "log10", limits = c(10, 200)) +
   scale_y_continuous(transform = scales::compose_trans("log10", "reverse"))
 ggMarginal(g4, type = "histogram", size = 3)
-
-
-
 
 
 # Pêche de loisir ----

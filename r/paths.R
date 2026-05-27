4d1cd9d0c1a89c2c47cbd240e0e56c611956f9ad
aubin_woehrel <73119009+awoehrel@users.noreply.github.com>
2026-05-27 15:32:39 +0200

    small changes

--- r/paths.R
+++ r/paths.R
@@ -58,8 +58,8 @@ with(paths_env, {
 
   # Carroyage
   raw_carroyage <- "data/raw/carroyage/zone_biocenoses/"
-  raw_carroyage_final <- "data/raw/carroyage/zone_biocenoses/grille_talassa_2025_cotier_hexagone_cinquiemedemile.gpkg"
-  raw_carroyage_carpediem <- "data/raw/carroyage/zone_biocenoses/grille_carpediem_1m_2019_pnmcca_cotier.gpkg"
+  raw_carroyage_hexcinquieme <- "data/raw/carroyage/zone_biocenoses/grille_talassa_2025_cotier_hexagone_cinquiemedemile.gpkg"
+  raw_carroyage_rect1mile <- "data/raw/carroyage/zone_biocenoses/grille_carpediem_1m_2019_pnmcca_cotier.gpkg"
 
   ### Référentiels codes et intitulés ----
   raw_codes_survolusage <- "data/raw/codes/reference_codes_survolusages_resoblo.csv"
